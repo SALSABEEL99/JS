a0a66bcce5f3bbc3edc1806213cffef1c5b0f816
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-05-16 02:55:53 +0500

    operators and if else ternary statement complete

--- app.js
+++ app.js
@@ -8,4 +8,113 @@
 
 };
 console.log(typeof profile['followers'])
-//object//*/
+//object//
+
+
+//Conditional statement
+// if statement 
+let age = 16;
+
+if (age >= 18) {
+ console.log("you can vote");   //false no return
+}
+
+if (age < 18) {
+    console.log("you cannot vote"); // true return 
+}
+
+if (mode == 'dark') {
+    color ='black'
+}
+
+if (mode =='light') {
+    color='white'
+}
+console.log(color);
+//if return true value //
+
+
+
+//if/else statement//
+
+let mode = 'light';
+let color;
+if (mode=='dark') {   //Id true value return
+    color = 'black'
+} else {
+    color = 'white'   //Else false value return
+}
+console.log(color);
+
+let age = 21;
+if (age >= 18) {
+    console.log('vote dega');
+} else {
+    console.log('nhe dega');
+}
+
+//1.3.5 odd number
+//2.4.6 even number
+//even odd
+let num = 12;
+
+if (num % 2==0) {
+    console.log(num,'is  even');
+} else {
+ console.log(num,'is odd');   
+}
+
+
+//ELSE IF STATEMENT  
+let mode = "dark";
+let color;
+
+
+if (mode==='dark') {
+    color='black'
+}
+else if (mode==='white'){
+    color='light'
+}
+else if (mode==='green'){
+    color='flag'
+}
+else{
+ color='grey'   
+}
+
+console.log(color);// agr koi if ya else if nhe chale to last wala else auto chaliga
+
+//Ternary operators
+let age = 25;
+
+let result = age >= 16 ? 'adult' : 'not adult';
+
+console.log(result);// compact if else ternary opretaor
+// practise question 1
+
+let num = prompt('enter a number');
+
+if (num % 5===0) {
+    console.log(num,'no is multiple of 5');
+}else{
+    console.log(num,'no is not multuple of 5');
+}
+//practsie question 2 
+let score = prompt('enter your score (0-100');// change score to change a grade
+let grade;
+if (score >= 90 && score <= 100) {
+    grade='A';
+}else if(score >= 70 && score <= 89) {
+    grade='B';
+}else if (score >= 60 && score <= 69){
+    grade='C'
+}else if(score >= 50 && score <= 59){
+    grade = 'D'
+}else if(score >= 0 && score <= 49){
+    grade = 'F'
+}
+console.log('according to your scores your grades was',grade); */
+
+
+
