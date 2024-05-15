0fee85356c1571e9a0f37acb8afd574b65aa912e
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-05-15 16:21:11 +0500

    Varibles and Data types complete with practice

---
+++ app.js
@@ -0,0 +1,11 @@
+/*const profile = { 
+    userName: "SalSabeel",
+    following: 217,
+    isFollow : true,
+    followers : 270,
+    posts :0,
+    bio : "nothing but everything banuk"
+
+};
+console.log(typeof profile['followers'])
+//object//*/
